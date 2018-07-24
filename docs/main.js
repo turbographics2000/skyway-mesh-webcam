9aecbe4bd523d23a8a77e1e9b7a4be1d1b4e07f8
turbographics2000 <turbographics2000@yahoo.co.jp>
2018-07-24 16:15:16 +0900

    Create main.js

---
+++ docs/main.js
@@ -0,0 +1,20 @@
+const myId = (new MediaStream).id;
+console.log(`myId:${myId}`);
+let stream = null;
+navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+    console.log(`streamId:${stream.id}`);
+    localView.srcObject = stream;
+    const peer = new Peer(myId, {
+        key: '01099bd8-1083-4c33-ba9b-564a1377e901'
+    });
+    peer.on('open', id => {
+        myIdDisp.textContent = id;
+        const room = peer.joinRoom('hoge_fuga_piyo_mesh', { mode: 'mesh', stream });
+        room.on('stream', stream => {
+            console.log(`room on stream peerId:${stream.peerId}`);
+            remoteView.srcObject = stream;
+        });
+    });
+}).catch(err => {
+    console.error(err);
+});
